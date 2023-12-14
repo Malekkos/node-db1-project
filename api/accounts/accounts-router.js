294c21ac7ba58ebba6c0409aa81565f10b991491
Christian Anderson <tehninjawaffle@gmail.com>
2023-12-14 11:41:38 -0800

    Imported account model, used account model in getAll, successfully gets all. Setup error handling next statement. Imported router inside of server.js and made it use with the pathway of '/api/accounts'

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,7 +1,15 @@
 const router = require('express').Router()
+const Account = require("./accounts-model")
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  Account.getAll()
+  .then(accounts => {
+    res.status(200).json(accounts)
+  })
+  .catch(error => {
+    next(error)
+  })
 })
 
 router.get('/:id', (req, res, next) => {
@@ -20,8 +28,12 @@ router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
 })
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.use((error, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(error.status || 500).json({
+    message: error.message,
+    customeMessage: "Something bad inside the accounts router"
+  })
 })
 
 module.exports = router;
